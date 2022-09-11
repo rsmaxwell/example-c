f972c88c9d21f41b1bb0713019b38fd50b30e494
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-09-11 10:49:46 +0100

    add debug to deploy script
    resolve merge conflicts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,22 @@ URL=https://pluto.rsmaxwell.co.uk/archiva/repository/${REPOSITORY}
 
 ZIPFILE=${ARTIFACTID}.${PACKAGING}
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$(dirname ${SCRIPT_DIR})
+BUILD_DIR=${PROJECT_DIR}/build
+PACKAGE_DIR=${PROJECT_DIR}/package
+
+cd ${PACKAGE_DIR}
+
+
+echo "-----[ debug info ]----------------"
+set -x 
+ls -al ${HOME}
+ls -al ${HOME}/.m2
+cat ${HOME}/.m2/settings.xml
+pwd
+ls -al 
+
 mvn --batch-mode deploy:deploy-file \
 	-DgroupId=${GROUPID} \
 	-DartifactId=${ARTIFACTID} \
