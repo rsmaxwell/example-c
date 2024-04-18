a6892b571d0f8b0932002cdae04fb46783ae6eb2
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-04-18 12:32:37 +0000

    update build scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,15 +14,22 @@ DIST_DIR=${PROJECT_DIR}/dist
 GROUPID=com.rsmaxwell.example
 ARTIFACTID=${PROJECT}_${FAMILY}_${ARCHITECTURE}
 PACKAGING=zip
-URL=https://pluto.rsmaxwell.co.uk/archiva/repository/${REPOSITORY}
 ZIPFILE=${ARTIFACTID}_${VERSION}.${PACKAGING}
 
 cd ${DIST_DIR}
 
-id -a 
-ls -al ~
-ls -al ~/.m2
-cat ~/.m2/settings.xml
+if [ -f ${HOME}/.m2/maven-repository-info ]; then
+    . ${HOME}/.m2/maven-repository-info
+elif [ -f ./maven-repository-info ]; then
+    . ./maven-repository-info
+fi
+
+if [ -z "${MAVEN_REPOSITORY_BASE_URL}" ]; then
+    echo "'MAVEN_REPOSITORY_BASE_URL' is not defined"
+    exit 1
+fi
+
+REPOSITORY_URL="${MAVEN_REPOSITORY_BASE_URL}/${REPOSITORY}"
 
 
 mvn --batch-mode --errors deploy:deploy-file \
@@ -32,5 +39,5 @@ mvn --batch-mode --errors deploy:deploy-file \
 	-Dpackaging=${PACKAGING} \
 	-Dfile=${ZIPFILE} \
 	-DrepositoryId=${REPOSITORYID} \
-	-Durl=${URL}
+	-Durl=${REPOSITORY_URL}
 
