f9020b588b1ffbdd1fcc3f3dc2373ecd3a9660d8
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-04-19 07:45:16 +0000

    update build scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
 
-if [ -z "${BUILD_ID}" ]; then
-    BUILD_ID="(none)"
-    VERSION="0.0.1-SNAPSHOT"
-    REPOSITORY=snapshots
-    REPOSITORYID=snapshots
-else
-    VERSION="0.0.1.$((${BUILD_ID}))"
-    REPOSITORY=releases
-    REPOSITORYID=releases
-fi
-
-
 BASEDIR=$(dirname "$0")
 SCRIPT_DIR=$(cd $BASEDIR && pwd)
 PROJECT_DIR=$(dirname $SCRIPT_DIR)
@@ -25,6 +13,16 @@ mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 
+if [ -z "${BUILD_ID}" ]; then
+    BUILD_ID="(none)"
+    VERSION="0.0.1-SNAPSHOT"
+    REPOSITORY=snapshots
+    REPOSITORYID=snapshots
+else
+    VERSION="0.0.1.$((${BUILD_ID}))"
+    REPOSITORY=releases
+    REPOSITORYID=releases
+fi
 
 
 cat > versioninfo <<EOL
@@ -45,6 +43,9 @@ GIT_COMMIT="${GIT_COMMIT:-(none)}"
 GIT_BRANCH="${GIT_BRANCH:-(none)}"
 GIT_URL="${GIT_URL:-(none)}"
 
+export PROJECT
+export REPOSITORY
+export REPOSITORYID
 export BUILD_ID
 export VERSION
 export TIMESTAMP
@@ -52,7 +53,7 @@ export GIT_COMMIT
 export GIT_BRANCH
 export GIT_URL
 
-tags='$VERSION,$BUILD_ID,$TIMESTAMP,$GIT_COMMIT,$GIT_BRANCH,$GIT_URL'
+tags='$PROJECT,$REPOSITORY,$REPOSITORYID,$VERSION,$BUILD_ID,$TIMESTAMP,$GIT_COMMIT,$GIT_BRANCH,$GIT_URL'
 
 cd ${TEMPLATES_DIR}
 
